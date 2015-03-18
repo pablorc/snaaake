bb85f1522e2477d229739e47a5a238d53ea8a963
Pablo Rivera Conde <pabloriveraconde@gmail.com>
2015-03-18 20:53:23 +0100

    The snake is able to move itself

--- test/snake_test.js
+++ test/snake_test.js
@@ -7,28 +7,45 @@ describe("Snake", function(){
     it("should be able to be created", function(){
       timer = new Timer();
       chai.expect(new Snake({
-        timer: timer
+        timer: timer,
+        board: new Board()
       })).to.be.a('object');
     });
 
     it("should assign properly the timer", function(){
       var timer = new Timer();
       var snake = new Snake({
-        timer: timer
+        timer: timer,
+        board: new Board()
       });
       chai.expect(snake.get("timer")).to.eql(timer);
-
-    });
+});
 
     it("should move when the timer tells it", function(){
       var timer = new Timer();
       var snake = new Snake({
-        timer: timer
+        timer: timer,
+        board: new Board()
       });
       var spy = sinon.spy(snake, "move_it");
       timer.tick();
       chai.expect(spy).to.be.called;
     })
+  });
+
+  describe(".move", function(){
+    it("should move itself to the next coords", function(){
+      var board = new Board();
+      var stub = sinon.stub(board, "move")
+      stub.withArgs([0,0], "up").returns([4,4]);
+      var snake = new Snake({
+        timer: new Timer(),
+        board: board
+      });
+
+      snake.move_it()
+      chai.expect(snake.get("coords")).to.be.eql([4,4])
+    });
   });
 
 });
--- www/js/snake.js
+++ www/js/snake.js
@@ -1,10 +1,18 @@
 Snake = Backbone.Model.extend({
+  
+  defaults: {
+    size: 1,
+    coords: [0,0],
+    direction: "up"
+  },
+
   initialize: function(attrs){
-    this.set("size", 3);
     this.listenTo(attrs.timer, "ticktack", this.move_it);
   },
 
   move_it: function(){
+    var new_coords = this.get("board").move(this.get("coords"), this.get("direction"));
+    this.set("coords", new_coords);
   }
 });
 
