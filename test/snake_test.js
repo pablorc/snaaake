0e483c783fb177aef3269feab46efc064774e39e
Pablo Rivera Conde <pabloriveraconde@gmail.com>
2015-03-18 19:59:23 +0100

    Testing listenTos

--- test/snake_test.js
+++ test/snake_test.js
@@ -1,14 +1,41 @@
 require("./helpers");
 
+
 describe("Snake", function(){
 
   describe("initialization", function(){
     it("should be able to be created", function(){
       timer = new Timer();
-      chai.expect(new Snake(timer, {
+      chai.expect(new Snake({
+        timer: timer
+      }, {
         parse: true
       })).to.be.a('object');
     });
+
+    it("should assign properly the timer", function(){
+      var timer = new Timer();
+      var snake = new Snake({
+        timer: timer
+      },{
+        parse: true
+      });
+      chai.expect(snake.timer).to.eql(timer);
+
+    });
+
+    it("should move when the timer tells it", function(){
+      var timer = new Timer();
+      var snake = new Snake({
+        timer: timer
+      },{
+        parse: true
+      });
+      var spy = sinon.spy(snake, "move_it");
+      timer.tick();
+      chai.expect(spy).to.be.called;
+    })
   });
+
 });
 
--- www/js/snake.js
+++ www/js/snake.js
@@ -1,11 +1,15 @@
 Snake = Backbone.Model.extend({
   initialize: function(attrs){
     this.set("size", 3);
+    this.listenTo(timer, "ticktack", this.move_it);
   },
 
   parse: function(attrs){
     this.timer = attrs.timer;
     return _(attrs).omit("timer");
+  },
+
+  move_it: function(){
   }
 });
 
