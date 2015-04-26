655a81b6f7e2a9035d06348b933614fa66aca009
Pablo Rivera Conde <pabloriveraconde@gmail.com>
2015-04-26 21:54:09 +0200

    Creating base models in main.js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,17 +2,24 @@
 
 
 window.ShakeTheSnake = {
-    Models: {},
-    Collections: {},
-    Views: {},
-    Routers: {},
-    init: function () {
-        'use strict';
-        console.log('Hello from Backbone!');
-    }
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  init: function () {
+    'use strict';
+
+    var board = new ShakeTheSnake.Models.Board();
+    var timer = new ShakeTheSnake.Models.Timer();
+    var snake = new ShakeTheSnake.Models.Snake({
+      board: board,
+      timer: timer
+    });
+    console.log(board, timer, snake);
+  }
 };
 
 $(document).ready(function () {
-    'use strict';
-    ShakeTheSnake.init();
+  'use strict';
+  ShakeTheSnake.init();
 });
