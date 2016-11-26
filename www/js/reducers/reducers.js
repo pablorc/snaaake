ece5d160bf9f32222f837d0412edf9620494b187
Pablo Rivera Conde <pabloriveraconde@gmail.com>
2016-11-26 14:43:26 +0100

    Create general reducer

--- www/js/reducers/reducers.js
+++ www/js/reducers/reducers.js
@@ -1,8 +1,10 @@
 import { snake } from './snake';
 
-const reducer = (state, action) => {
+const reducer = (state = {}, action) => {
   return {
     snake: snake(state.snake, action),
     foods: []
   }
 }
+
+export default reducer;
