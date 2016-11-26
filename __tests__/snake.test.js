c28768e69d73f2450e71566d1fd07432f3e76a20
Pablo Rivera Conde <pabloriveraconde@gmail.com>
2016-11-26 18:37:30 +0100

    Add tests for snake

--- __tests__/snake.test.js
+++ __tests__/snake.test.js
@@ -11,14 +11,16 @@ test('Gets initial state', () => {
 test('Moves to the next pixel down', () => {
   const stateBefore = {
     position: [[1,1], [1,2]],
-    head: 'down'
+    head: 'down',
+    justEat: false
   };
   const action = {
     type: 'MOVE'
   }
   const stateAfter = {
     position: [[1,2], [2,2]],
-    head: 'down'
+    head: 'down',
+    justEat: false
   }
 
   expect(snake(stateBefore, action)).toEqual(stateAfter);
@@ -27,14 +29,16 @@ test('Moves to the next pixel down', () => {
 test('Moves to the next pixel right', () => {
   const stateBefore = {
     position: [[1,1], [1,2]],
-    head: 'right'
+    head: 'right',
+    justEat: false
   };
   const action = {
     type: 'MOVE'
   }
   const stateAfter = {
     position: [[1,2], [1,3]],
-    head: 'right'
+    head: 'right',
+    justEat: false
   }
 
   expect(snake(stateBefore, action)).toEqual(stateAfter);
@@ -43,14 +47,16 @@ test('Moves to the next pixel right', () => {
 test('Moves to the next pixel up', () => {
   const stateBefore = {
     position: [[1,1], [1,2]],
-    head: 'up'
+    head: 'up',
+    justEat: false
   };
   const action = {
     type: 'MOVE'
   }
   const stateAfter = {
     position: [[1,2], [0,2]],
-    head: 'up'
+    head: 'up',
+    justEat: false
   }
 
   expect(snake(stateBefore, action)).toEqual(stateAfter);
@@ -59,14 +65,16 @@ test('Moves to the next pixel up', () => {
 test('Moves to the next pixel left', () => {
   const stateBefore = {
     position: [[2,2], [1,2]],
-    head: 'left'
+    head: 'left',
+    justEat: false
   };
   const action = {
     type: 'MOVE'
   }
   const stateAfter = {
     position: [[1,2], [1,1]],
-    head: 'left'
+    head: 'left',
+    justEat: false
   }
 
   expect(snake(stateBefore, action)).toEqual(stateAfter);
@@ -75,7 +83,8 @@ test('Moves to the next pixel left', () => {
 test('Changes direction', () => {
   const stateBefore = {
     position: [[2,2], [1,2]],
-    head: 'left'
+    head: 'left',
+    justEat: false
   };
   const action = {
     type: 'CHANGE_DIR',
@@ -83,8 +92,44 @@ test('Changes direction', () => {
   }
   const stateAfter = {
     position: [[2,2], [1,2]],
-    head: 'down'
+    head: 'down',
+    justEat: false
   }
 
   expect(snake(stateBefore, action)).toEqual(stateAfter);
+});
+
+test('Adds a justEat flag after eat', () => {
+  const stateBefore = {
+    position: [[2,2], [1,2]],
+    head: 'left'
+  };
+  const action = {
+    type: 'EAT'
+  };
+  const stateAfter = {
+    position: [[2,2], [1,2]],
+    head: 'left',
+    justEat: true
+  };
+
+  expect(snake(stateBefore, action)).toEqual(stateAfter);
+});
+
+test('Movement removes a justEat flag and makes the snake bigger', () => {
+  const stateBefore = {
+    position: [[2,2], [1,2]],
+    head: 'left',
+    justEat: true
+  };
+  const action = {
+    type: 'MOVE'
+  };
+  const stateAfter = {
+    position: [[2,2], [1,2], [1,1]],
+    head: 'left',
+    justEat: false
+  };
+
+  expect(snake(stateBefore, action).justEat).toBe(false);
 });
