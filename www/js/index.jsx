1b4d856452aedc5d597f05599dd194b7f80c45b3
Pablo Rivera Conde <pabloriveraconde@gmail.com>
2016-11-29 21:16:16 +0100

    Add VIM controls support (hjkl)

--- www/js/index.jsx
+++ www/js/index.jsx
@@ -33,10 +33,16 @@ window.addEventListener("keydown", event => {
     38: 'up',
     39: 'right',
     40: 'down',
+
     65: 'left',  // w
     87: 'up',    // a
     68: 'right', // s
-    83: 'down'   // d
+    83: 'down',   // d
+
+    72: 'left',  // h
+    74: 'down',  // j
+    75: 'up',    // k
+    76: 'right'  // l
   };
   if( movement[event.keyCode] ) {
     store.dispatch({
