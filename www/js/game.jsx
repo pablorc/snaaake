3cd1e2bbd84c317b585599d6cfc86fadd419988c
Pablo Rivera Conde <pabloriveraconde@gmail.com>
2016-11-29 21:57:27 +0100

    Improve a bit the layout

--- www/js/game.jsx
+++ www/js/game.jsx
@@ -13,11 +13,11 @@ const Game =  React.createClass({
     return (
       <div className="game">
         <h1>Snaaake</h1>
+        <span className="game__score">{this.props.store.score.score}</span>
         <Maze width={30} height={30}>
           <Snake position={this.props.store.snake.position}/>
           <Food x={this.props.store.food.x} y={this.props.store.food.y} />
         </Maze>
-        <span>{this.props.store.score.score}</span>
       </div>
     );
   }
