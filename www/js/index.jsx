e7cc27679909a9f392fb5f0ffbbf6115ad94c9dc
Pablo Rivera Conde <pabloriveraconde@gmail.com>
2016-12-25 12:03:35 +0100

    Refactor controls to its own file

--- www/js/index.jsx
+++ www/js/index.jsx
@@ -7,10 +7,12 @@ import { createStore } from 'redux';
 import Game from './game';
 import reducer from './reducers/reducers';
 import { setupColliders } from './colliders';
+import { setupControls } from './controls';
 
 const store = createStore(reducer);
 
 setupColliders(store);
+setupControls(store);
 
 const render = () => {
   ReactDOM.render(
@@ -25,31 +27,6 @@ render();
 // Timer
 const intervalId = setInterval( () => store.dispatch({type: 'MOVE'}), 50);
 
-// Keyboard control
-window.addEventListener("keydown", event => {
-  const movement = {
-    37: 'left',
-    38: 'up',
-    39: 'right',
-    40: 'down',
-
-    65: 'left',  // w
-    87: 'up',    // a
-    68: 'right', // s
-    83: 'down',   // d
-
-    72: 'left',  // h
-    74: 'down',  // j
-    75: 'up',    // k
-    76: 'right'  // l
-  };
-  if( movement[event.keyCode] ) {
-    store.dispatch({
-      type: 'CHANGE_DIR',
-      direction: movement[event.keyCode]
-    });
-  }
-}, true);
 
 const gameOver = () => {
     console.log('GAME OVER');
