91890f51c56c4e75d55bcc311df5072929e44559
Pablo Rivera Conde <pabloriveraconde@gmail.com>
2017-05-13 12:33:25 +0200

    Add tests

--- __tests__/game.test.js
+++ __tests__/game.test.js
@@ -14,3 +14,32 @@ test('EAT adds 100 points to score', () => {
   expect(game(stateBefore, action)).toEqual(stateAfter);
 });
 
+test('GAMEOVER finish the game', () => {
+  const stateBefore = {
+    running: true
+  }
+  const action = {
+    type: 'GAMEOVER'
+  }
+  const stateAfter = {
+    running: false
+  }
+
+  expect(game(stateBefore, action)).toEqual(stateAfter);
+});
+
+test('GAMEOVER does not add score', () => {
+  const stateBefore = {
+    score: 200,
+    running: true
+  }
+  const action = {
+    type: 'GAMEOVER'
+  }
+  const stateAfter = {
+    score: 200,
+    running: false
+  }
+
+  expect(game(stateBefore, action)).toEqual(stateAfter);
+});
